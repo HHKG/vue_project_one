935ca20f44cc3d91cf265f15182d464cf3254061
HHKG <972834061@qq.com>
2020-05-27 18:20:25 +0800

    lll

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,53 +1,59 @@
 <template>
-  <div>
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-container>
-          <el-main style="height: 100wh;">Main</el-main>
-          <el-footer>Footer</el-footer>
-        </el-container>
-      </el-container>
-    </el-container>
+  <div class="manage_page fillcontain">
+    <el-row style="height: 100%;">
+    	<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
+    		<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router>
+    			<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+    			<el-submenu index="2">
+    				<template slot="title"><i class="el-icon-document"></i>数据管理</template>
+    				<el-menu-item index="userList">用户列表</el-menu-item>
+    				<el-menu-item index="shopList">商家列表</el-menu-item>
+    				<el-menu-item index="foodList">食品列表</el-menu-item>
+    				<el-menu-item index="orderList">订单列表</el-menu-item>
+    				<el-menu-item index="adminList">管理员列表</el-menu-item>
+    			</el-submenu>
+    			<el-submenu index="3">
+    				<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
+    				<el-menu-item index="addShop">添加商铺</el-menu-item>
+    				<el-menu-item index="addGoods">添加商品</el-menu-item>
+    			</el-submenu>
+    			<el-submenu index="4">
+    				<template slot="title"><i class="el-icon-star-on"></i>图表</template>
+    				<el-menu-item index="visitor">用户分布</el-menu-item>
+    				<!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
+    			</el-submenu>
+    			<el-submenu index="5">
+    				<template slot="title"><i class="el-icon-edit"></i>编辑</template>
+    				<!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
+    				<el-menu-item index="vueEdit">文本编辑</el-menu-item>
+    			</el-submenu>
+    			<el-submenu index="6">
+    				<template slot="title"><i class="el-icon-setting"></i>设置</template>
+    				<el-menu-item index="adminSet">管理员设置</el-menu-item>
+    				<!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
+    			</el-submenu>
+    			<el-submenu index="7">
+    				<template slot="title"><i class="el-icon-warning"></i>说明</template>
+    				<el-menu-item index="explain">说明</el-menu-item>
+    			</el-submenu>
+    		</el-menu>
+    	</el-col>
+    	<el-col :span="20" style="height: 100%;overflow: auto;">
+    		<keep-alive>
+                <!-- vue提供组件keep-alive组件是为了做当前页面的缓存，在离开该页面去到别的页面的时候，再回来的时候，回到上次离开页面时的一个状态 -->
+    		    <router-view></router-view>
+    		</keep-alive>
+    	</el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 </script>
 
-<style>
-  .el-header, .el-footer {
-      background-color: #B3C0D1;
-      color: #333;
-      text-align: center;
-      line-height: 60px;
-    }
+<style scoped="scoped">
+/*  @import '../style/mixin';
+  .manage_page{
 
-    .el-aside {
-      background-color: #D3DCE6;
-      color: #333;
-      text-align: center;
-      line-height: 200px;
-    }
-
-    .el-main {
-      background-color: #E9EEF3;
-      color: #333;
-      text-align: center;
-      line-height: 160px;
-    }
-
-    body > .el-container {
-      margin-bottom: 40px;
-    }
-
-    .el-container:nth-child(5) .el-aside,
-    .el-container:nth-child(6) .el-aside {
-      line-height: 260px;
-    }
-
-    .el-container:nth-child(7) .el-aside {
-      line-height: 320px;
-    }
+  } */
 </style>
